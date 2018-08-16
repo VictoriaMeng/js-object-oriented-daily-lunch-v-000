f02f4e3a40fd76cb558c38f150e6440b602473d9
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 22:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ class Meal {
 
 class Delivery {
  constructor(mealId, neighborhoodId, customerId) {
-   
+   this.mealId = mealid;
+   this.neighborhoodId = neighborhoodId;
+   this.customerId = customerId;
  };
 };
